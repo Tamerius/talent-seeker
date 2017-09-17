898fb613cb697e758f573eb5e00ab01e42f7831f
Yannick <yannick_tamerius@hotmail.com>
2017-09-17 15:51:10 +0200

    add auth

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UsersController extends Controller
+{
+	public function index()
+	{
+	    $users = [
+			'0' => [
+				'first_name' => 'Renato',
+				'last_name' => 'Hysa',
+				'location' => 'Albania'
+			],
+			'1' => [
+				'first_name' => 'Jessica',
+				'last_name' => 'Alba',
+				'location' => 'USA'
+			]
+		];
+		return view('admin.users.index', compact('users'));
+	}
+
+	public function create()
+	{
+		return view('admin.users.create');
+	}
+
+	public function store(Request $request)
+	{
+		User::create($request->all());
+		return 'success';
+	}
+}
