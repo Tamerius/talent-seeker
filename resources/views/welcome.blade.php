e2fcf4078c8ff3319ad88d7bedac755b48144dd4
Yannick <yannick_tamerius@hotmail.com>
2017-09-24 22:08:19 +0200

    change index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Talent-Seeker
                 </div>
 
                 <div class="links">
